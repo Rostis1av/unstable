8c8bdfae279e961cacb622225b6d2967d749a7ac
Rostislav G. Bronzov <rbronzov@gmail.com>
2019-10-14 17:28:23 +0300

    Add files via upload

---
+++ EUServiceMGMT/root.psm1
@@ -0,0 +1,6 @@
+﻿function global:prompt{
+    Write-Host ((Get-Date -f "HH:mm:ss")+" $env:USERDNSDOMAIN\$env:USERNAME") -ForegroundColor Yellow
+    Write-Host (Get-Location)">>>" -NoNewLine
+    $Host.UI.RawUI.WindowTitle=((Get-Date -f "HH:mm:ss")+" $env:USERDNSDOMAIN\$env:USERNAME")
+    return " "
+}
---
+++ EUServiceMGMT/tools.psm1
@@ -0,0 +1,27 @@
+﻿function Set-ServiceLogonName {
+[CmdletBinding()]
+PARAM (
+    [parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [string[]]$ServiceName,
+    [string]$LogonName="LocalSystem",
+    [string]$LogonPassword=$null,
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false)]
+    [string]$ComputerName="localhost"
+)
+
+BEGIN {}
+
+PROCESS {
+    foreach ($service in $ServiceName) {
+        $Parameters=@{
+            'StartName'="$LogonName";
+            'StartPassword'="$LogonPassword"
+        }
+        Invoke-CimMethod -Query "SELECT * FROM Win32_Service WHERE Name=`'$service`'" -Method Change -Arguments $Parameters -Computername $ComputerName | Out-Null
+        $result = Get-CimInstance win32_service -Filter "name like `"$service`"" -ComputerName $ComputerName | select -Property Name,SystemName,StartName
+        return $result
+    } 
+}
+
+END {}
+}
